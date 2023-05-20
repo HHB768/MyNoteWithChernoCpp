e2512b8ac5448f48bb3dacbf21fd53e21b81ebcb
HHB <61405138+HHB768@users.noreply.github.com>
2023-05-20 16:46:25 +0800

    Create _4_functions.cpp

---
+++ _4_functions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int multiply1(int a, int b)
+{
+	return a * b;
+}
+
+
+void multiandlog(int a, int b)
+{
+	std::cout << multiply1(a, b) << std::endl;
+}
+
+int main()
+{
+	//std::cout << multiply1(2, 3) << std::endl;
+	//std::cout << multiply1(5, 8) << std::endl;
+	//std::cout << multiply1(10, 15) << std::endl;
+	// too risky
+	multiandlog(2, 3);
+	multiandlog(5, 8);
+	multiandlog(10, 15);
+}
