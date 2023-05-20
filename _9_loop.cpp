9ed927d026607043bff8c3ebd7fd7c38a4a7003d
HHB <61405138+HHB768@users.noreply.github.com>
2023-05-20 16:56:05 +0800

    Create _9_loop.cpp

---
+++ _9_loop.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "log.h"
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		logr("hello world");
+	}
+
+	// same
+	//int i = 0;
+	//for ( ; i < 5; )  // if you set for (;;), it's equal to for (;true;)
+	//{
+	//	logr("hello world");
+	//	i++;
+	//}
+
+	int i = 0;
+	while (i < 5)
+	{
+		logr("niganma-aiyo");
+		i++;
+	}
+
+	std::cin.get();
+}
